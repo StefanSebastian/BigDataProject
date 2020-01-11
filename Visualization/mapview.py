7e83cc0da61d392f089c02d0d956dae4828a526d
StefanSebastian <stefansebii@gmail.com>
2020-01-11 12:43:13 +0200

    Removed unnecessary dependency for map visualization

--- Visualization/mapview.py
+++ Visualization/mapview.py
@@ -1,6 +1,5 @@
 # import libraries
 import folium
-import pandas as pd
 from cassandra.cluster import Cluster
 
 cluster = Cluster(['localhost'],port=9042)
@@ -16,19 +15,12 @@ for row in rows:
     lon.append(row.longitude)
     labels.append(row.type)
  
-# Make a data frame with dots to show on the map
-data = pd.DataFrame({
-'lat':lat,
-'lon':lon,
-'name':labels
-})
- 
 # Make an empty map, roughly around Romania
 m = folium.Map(location=[45.704181,24.0047547], zoom_start=7.08)
  
 # Add data picked up from cassandra
-for i in range(0,len(data)):
-    folium.Marker([data.iloc[i]['lat'], data.iloc[i]['lon']], popup=data.iloc[i]['name']).add_to(m)
+for i in range(0,len(labels)):
+    folium.Marker([lat[i], lon[i]], popup=labels[i]).add_to(m)
 
 # Save it as html
 m.save('map_visualisation.html')
