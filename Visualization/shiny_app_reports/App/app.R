cc43e62b8715b3d6d4f833570db0693bd7475a2b
StefanSebastian <stefansebii@gmail.com>
2020-01-11 18:23:49 +0200

    Error code histogram

--- Visualization/shiny_app_reports/App/app.R
+++ Visualization/shiny_app_reports/App/app.R
@@ -9,14 +9,34 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel("sidebar panel"),
     mainPanel(
-      plotOutput("failures",height = 500)
+      plotOutput("failure_rate", height = 500),
+      plotOutput("code_histogram", height = 500)
     )
   )
 )
 
+
+
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  output$failures <- renderPlot({
+  output$code_histogram <- renderPlot({
+    genDb <- dbConnect(RMariaDB::MariaDB(), user='uf5xGXLgpR', password='id46xBiuhV', dbname='uf5xGXLgpR', host='remotemysql.com', port=3306)
+    query<-paste("SELECT 
+                  case 
+                      when e.code is not null then e.code + 1
+                    when e.code is NULL then 0
+                  end as code 
+                  FROM drone_status d LEFT JOIN error_occurence e on d.part_id = e.part_id ",sep="")
+    rs = dbSendQuery(genDb,query)
+    dbRows <- dbFetch(rs)
+    dbDisconnect(genDb)
+
+    dbRows = as.numeric(as.character(dbRows$code))
+    bins = c(0, 1, 2, 3)
+    hist(dbRows, main="Error code histogram", xlab="Code", freq=FALSE, breaks=bins, right=FALSE)
+  })
+
+  output$failure_rate <- renderPlot({
     genDb <- dbConnect(RMariaDB::MariaDB(), user='uf5xGXLgpR', password='id46xBiuhV', dbname='uf5xGXLgpR', host='remotemysql.com', port=3306)
     query<-paste("SELECT d.batch_id,
                   sum(case when e.code IS NOT NULL then 1 else 0 end) as error_occurences,
